4bdb454f7eed2004675bec1e89e70ac123e5b9fb
Heath Reynolds <heath.reynolds@gmail.com>
2022-11-05 13:12:55 -0500

    Jenkins Rule

--- main.tf
+++ main.tf
@@ -16,3 +16,21 @@ provider "nsxt" {
   retry_max_delay       = 5000
   retry_on_status_codes = [429]
 }
+
+resource "nsxt_policy_security_policy" "firewall_section" {
+  display_name = "Jenkins_Infra"
+  description  = "Firewall section created by Terraform"
+  scope        = [nsxt_policy_group.all_vms.path]
+  category     = "Infrastructure"
+  locked       = "false"
+  stateful     = "true"
+
+  # Allow communication to any VMs only on the ports defined earlier
+  rule {
+    display_name       = "Allow Jenkins Build Server"
+    description        = "In going rule"
+    action             = "ALLOW"
+    logged             = "false"
+    ip_version         = "IPV4"
+    sources            = "192.168.110.90"
+  }
