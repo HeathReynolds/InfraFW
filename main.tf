d8e851f881a5ea755825194c878fb65ddaaf8d0b
Heath Reynolds <heath.reynolds@gmail.com>
2022-11-05 12:25:05 -0500

    TF Provider

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    nsxt = {
+      source = "vmware/nsxt"
+    }
+  }
+}
+
+provider "nsxt" {
+  host                  = "nsxmgr-01a"
+  username              = "admin"
+  password              = "VMware1!VMware1!"
+  allow_unverified_ssl  = true
+  max_retries           = 10
+  retry_min_delay       = 500
+  retry_max_delay       = 5000
+  retry_on_status_codes = [429]
+}
